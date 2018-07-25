1817e9e028eaa375e366727d53506f513c06f88f
nickname5 <ipod6666apple@gmail.com>
2018-07-25 18:01:51 +0300

    second slider

--- js/carousel.js
+++ js/carousel.js
@@ -1,15 +1,76 @@
 $(document).on('ready', function() {
-  // $(".center").slick({
-  //   dots: true,
-  //   infinite: true,
-  //   centerMode: true,
-  //   slidesToShow: 5,
-  //   slidesToScroll: 3
-  // });
   $(".single-item").slick({
     infinite: true,
     dots: true,
     arrows: false,
     centerMode: true
+  });
+
+  $('.responsive').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    mobileFirst: true,
+    // autoplay: true,
+    // autoplaySpeed: 10000,
+    // centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: true,
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          arrows: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          arrows: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 100,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 });
